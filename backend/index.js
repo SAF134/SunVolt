542bb24d139a51e4d72e827abf416b1814190322
SAF134 <syauqiakmal137@gmail.com>
2026-04-14 12:07:38 +0700

    feat: add vercel configuration

--- backend/index.js
+++ backend/index.js
@@ -141,3 +141,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Server berjalan di port ${PORT}`);
 });
+
+// Wajib untuk Vercel (Serverless Function)
+module.exports = app;
